1494a429f6d19d39b74037d1dc48992eed29c325
Brandi Neal <nealb3@nku.edu>
2026-03-30 12:00:06 -0400

    Update Firebase configuration to use window object

--- public/assets/firebase-config.js
+++ public/assets/firebase-config.js
@@ -1,6 +1,4 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.LEADLOOP_FIREBASE_CONFIG = {
   apiKey: "AIzaSyAWNTnXn2yNXv5KSfVYCW_LH5uVvGtx1TU",
   authDomain: "chatitsbad.firebaseapp.com",
   projectId: "chatitsbad",
@@ -9,6 +7,7 @@ const firebaseConfig = {
   appId: "1:254399478035:web:f7a07d1f33ef47a87e02e6",
   measurementId: "G-NX92YKEQJR"
 };
+
 window.LEADLOOP_RUNTIME = {
   projectId: "chatitsbad",
   region: "us-central1"
